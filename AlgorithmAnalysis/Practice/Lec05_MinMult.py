794a4a1f25d85346238ddb1a951f65871311ce4d
seongjunChoi <csj000714@khu.ac.kr>
2021-04-05 22:21:20 +0900

    MinMult

--- AlgorithmAnalysis/Practice/Lec05_MinMult.py
+++ AlgorithmAnalysis/Practice/Lec05_MinMult.py
@@ -2,13 +2,13 @@ import Lec05_Utility as utility
 
 def order(p, i, j):
     if (i == j):
-        print("A", i)
+        print(f"A{i}", end=" ")
     else:
         k = p[i][j]
-        print("(")
-        order(i, k)
-        order(k+1, j)
-        print(")")
+        print("(", end="")
+        order(p, i, k)
+        order(p, k+1, j)
+        print(")", end="")
          
 d = [5,2,3,4,6,7,8]
 n = len(d)-1
@@ -16,17 +16,30 @@ n = len(d)-1
 m=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
 p=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
 
-def minmult (n, d, P):
-    M = [[0 for i in range(n+1)] for j in range(n+1)]
-    for i in range(1, n+1):
-        M[i][i] = 0
-    for diag in range(1, n):
-        for i in range(1, n-diag+1):
-            j = i + diag
-            M[i][j] = min(M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j])
-            P[i][j] = k # 최소치를 주는 k의 값
-    return M[1][n]
+for i in range(1, n+1):
+    m[i][i] = 0
+for diag in range(1, n):
+    for i in range(1, n-diag+1):
+        j = i + diag
+        M = 987654321
+        for k in range(i, j):
+            temp = m[i][k] + m[k+1][j] + d[i-1]*d[k]*d[j]
+            if M > temp:
+                M = temp
+                P = k # 최소치를 주는 k의 값
+        m[i][j] = M
+        p[i][j] = P
 
+# def minmult (n, d, P):
+#     M = [[0 for i in range(n+1)] for j in range(n+1)]
+#     for i in range(1, n+1):
+#         M[i][i] = 0
+#     for diag in range(1, n):
+#         for i in range(1, n-diag+1):
+#             j = i + diag
+#             M[i][j] = min(M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j])
+#             P[i][j] = k # 최소치를 주는 k의 값
+#     return M[1][n]
 
 utility.printMatrix(m)
 print()
