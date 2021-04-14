2884c197df06c54c85f1cb109f5f54cfdcf0bfea
seongjunChoi <csj000714@khu.ac.kr>
2021-04-14 12:46:12 +0900

    Greedy base Kruskal Algorithm

---
+++ AlgorithmAnalysis/Practice/Lec07_GreedyKruskal.py
@@ -0,0 +1,44 @@
+parent = dict()
+rank = dict()
+
+def make_singleton_set(v):
+    parent[v] = v
+    rank[v] = 1
+
+def find(v):
+    if parent[v] != v:
+        parent[v] = find(parent[v])
+    return parent[v]
+
+
+def union(r1, r2):
+
+
+    if r1 != r2:
+        if rank[r1] > rank[r2]:
+            parent[r2] = r1
+            rank[r1] += rank[r2]
+        else:
+            parent[r1] = r2
+            if rank[r1] == rank[r2]: rank[r2] += rank[r1]
+# Kruskal algorithm 구현
+def kruskal(graph):
+
+
+
+
+
+graph = {
+        'vertices': ['A', 'B', 'C', 'D', 'E'],
+        'edges': set([
+            (1, 'A', 'B'),
+            (3, 'A', 'C'),
+            (3, 'B', 'C'),
+            (6, 'B', 'D'),
+            (4, 'C', 'D'),
+            (2, 'C', 'E'),
+            (5, 'D', 'E'),
+            ])
+        }
+mst=kruskal(graph)
+print(mst)
