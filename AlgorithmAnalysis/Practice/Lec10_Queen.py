f691051833439eeb4eac5f624b3175f63dbb1107
seongjunChoi <csj000714@khu.ac.kr>
2021-05-04 21:45:23 +0900

    Queen

---
+++ AlgorithmAnalysis/Practice/Lec10_Queen.py
@@ -0,0 +1,22 @@
+def promising(i,col):
+    k = 0
+    switch = True
+    while(k < i and switch):
+        if col[i] == col[k] or abs(col[i] - col[k]) == i-k:
+            switch = False
+        k += 1
+        
+    return switch
+
+def queens(n,i,col):
+    if promising(i,col):
+        if i+1 == n:
+            print(col)
+        else:
+            for j in range(0,n):
+                col[i+1] = j
+                queens(n,i+1,col)
+
+n = 5
+col=n*[0]
+queens(n,-1,col)
