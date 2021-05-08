c91edd8a9bdf59011d25e7d4f2fb645f2c8da014
seongjunChoi <csj000714@khu.ac.kr>
2021-05-08 18:43:19 +0900

    Level2_문자열압축

---
+++ Algorithm_Programmers/Level2/Level2_문자열압축.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = s.size();
+
+    for (int i = 1; i <= s.size() / 2; i++) {
+        string convert, temp;
+
+        int cnt = 1;
+        temp = s.substr(0, i);
+
+        for (int j = i; j < s.size(); j += i) {
+            if (temp == s.substr(j, i)) cnt++;
+            else {
+                if (cnt > 1) convert += to_string(cnt);
+                convert += temp;
+                temp = s.substr(j, i);
+                cnt = 1;
+            }
+        }
+
+        if (cnt > 1) convert += to_string(cnt);
+
+        convert += temp;
+        answer = (answer > convert.size()) ? convert.size() : answer;
+    }
+    return answer;
+}
+int main() {
+    string s = "aabbacc";
+    cout << solution(s);
+}
+
+//while (s[i] == s[i + cnt] && s[i + num] == s[i + cnt + num] && i + num != cnt) {
+//    num++;
+//}
