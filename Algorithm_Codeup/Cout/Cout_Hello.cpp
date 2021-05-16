dc30aaebb7a08b45a0a8217b27b31b5f1e48f693
DrawingProcess <csj000714@khu.ac.kr>
2021-05-16 17:00:34 +0900

    Codeup Cout

---
+++ Algorithm_Codeup/Cout/Cout_Hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "\"C:\\Download\\hello.cpp\"\n";
+	cout << "\u250C\u252C\u2510\n";
+	cout << "\u251C\u253C\u2524\n";
+	cout << "\u2514\u2534\u2518\n";
+}
