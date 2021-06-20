3d83a24afbc8b997320558b323758fab5d1f8d87
DrawingProcess <csj000714@khu.ac.kr>
2021-06-20 13:12:38 +0900

    Level2

---
+++ Algorithm_Programmers/Level2/Level2_순위검색.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<int> solution(vector<string> info, vector<string> query) {
+    vector<int> answer;
+    return answer;
+}
