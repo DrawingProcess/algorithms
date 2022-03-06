606d6943d90d1e2708780d6116e4cd7790ee241b
DrawingProcess <sjchoi.dp@gmail.com>
2022-03-06 20:59:18 +0900

    ps: Finish Level2_거리두리확인하기

--- Algorithm_Programmers/Level2/Level2_모음사전.cpp
+++ Algorithm_Programmers/Level2/Level2_모음사전.cpp
@@ -7,10 +7,19 @@ int answer = 0;
 
 int solution(string word)
 {
-    vector<char> alpha = {'A', 'E', 'I', 'O', 'U'};
-    do
+    int answer = 0;
+    int mul[5] = {1, 5, 25, 125, 625};
+    map<char, int> m = {{'A', 0}, {'E', 1}, {'I', 2}, {'O', 3}, {'U', 4}};
+    for (int i = 0; i < word.size(); i++)
     {
-        
-    } while (next_permutation(alpha.begin(), alpha.end()));
+        answer += 1;
+        for (int j = 0; j < m[word[i]]; j++)
+        {
+            for (int k = 4; k >= i; k--)
+            {
+                answer += mul[4 - k];
+            }
+        }
+    }
     return answer;
 }
