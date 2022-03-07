783d9e3562c0ab4d048aad2df60e1c4b334a944c
DrawingProcess <sjchoi.dp@gmail.com>
2022-03-07 18:29:24 +0900

    ps: Finish Level2

---
+++ Algorithm_Programmers/Level2/Level2_전화번호목록.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size()-1; i++)
+    {
+        if (phone_book[i] == phone_book[i+1].substr(0, phone_book[i].size()))
+        {
+            answer = false;
+        }
+    }
+    return answer;
+}
+int main(){
+    vector<string> num = {"12", "123", "1235", "567", "88"};
+    cout << solution(num);
+}
