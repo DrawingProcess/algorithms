2365193c89cf89c7111796c9388963617995df7c
Seongjun Choi <sjchoi.dp@gmail.com>
2023-09-15 18:37:45 +0900

    Solved 2075

---
+++ Algorithm_BaekjoonSol/datastructure/NthBiggestNum_2075.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n, num;
+    cin >> n;
+    for (int i = 0; i < n*n; i++)
+    {
+        cin >> num;
+        pq.push(num);
+        if (pq.size() > n)
+        {
+            pq.pop();
+        }
+    }
+    cout << pq.top();
+}
