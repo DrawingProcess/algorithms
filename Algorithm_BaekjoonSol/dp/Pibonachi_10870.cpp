fc4891579c7ba0e893810c765056438cd862b7e9
Seongjun Choi <sjchoi.dp@gmail.com>
2023-09-30 14:06:43 +0900

    Solved 10870

---
+++ Algorithm_BaekjoonSol/dp/Pibonachi_10870.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    int arr[21];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    cout << arr[n];
+}
